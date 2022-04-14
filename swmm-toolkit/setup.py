c68ebec029ade7fdb684245e714baf577fea521e
karosc <karosc@cdmsmith.com>
2022-04-14 14:32:55 -0400

    add 3.10 to setup.py

--- swmm-toolkit/setup.py
+++ swmm-toolkit/setup.py
@@ -116,10 +116,10 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
         "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: C",
         "Development Status :: 4 - Beta",
     ]
